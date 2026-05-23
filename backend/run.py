18b2a1f314be46358f89b2facfab9a27fd774bab
Luciano Gauna <luciano.927@hotmail.com>
2026-05-23 20:00:25 -0300

    feat: configurar servidor Flask inicial

---
+++ backend/run.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.get("/health")
+def health():
+    return {
+        "ok": True,
+        "message": "API Instruya Python MVC funcionando"
+    }, 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
